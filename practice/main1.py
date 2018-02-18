b55baf65ddf3a2e6290edbf549682f0f6b50f1dc
mgh3326 <mgh3326@naver.com>
2018-02-18 18:22:46 +0900

    azure

--- practice/main1.py
+++
@@ -1,8 +0,0 @@
-import mod
-
-clazz = mod.ClassName()
-clazz.memberAdd(5)
-clazz.memberAdd(5)
-clazz.memberPrint()
-
-print(mod.justAdd(3,5))
--- practice/main2.py
+++
@@ -1,9 +0,0 @@
-from mod import ClassName
-from mod import justAdd
-
-clazz = ClassName()
-clazz.memberAdd(5)
-clazz.memberAdd(5)
-clazz.memberPrint()
-
-print(justAdd(3,5))
